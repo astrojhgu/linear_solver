7d508a48299a5e94a12642a9be962bd4fd80c5df
Gu Junhua <jhgu@nao.cas.cn>
2019-12-12 16:04:31 +0800

    modified:   src/bin/test_arnoldi.rs

--- src/bin/test_arnoldi.rs
+++ src/bin/test_arnoldi.rs
@@ -15,13 +15,14 @@ pub fn main(){
     for i in 0..15{
         arn.iter(&A).unwrap();
     }
+
     //println!("{:?}",arn.Q);
-    let H=arn.get_H();
-    let Q=arn.get_Q();
-    RawMM::from_array2(H.view()).to_file("H.mtx");
-    RawMM::from_array2(Q.view()).to_file("Q.mtx");
+    //let H=arn.get_H();
+    //let Q=arn.get_Q();
+    //RawMM::from_array2(H.view()).to_file("H.mtx");
+    //RawMM::from_array2(Q.view()).to_file("Q.mtx");
 
     //let h1=qr::qr_naive_iter(H.slice(s![0..15,..]), 100);
-    let h1=qr::qr_with_shift(H.slice(s![0..15,..]), 1e-15);
-    RawMM::from_array2(h1.view()).to_file("h1.mtx");
+    //let h1=qr::qr_with_shift(H.slice(s![0..15,..]), 1e-15);
+    //RawMM::from_array2(h1.view()).to_file("h1.mtx");
 }
